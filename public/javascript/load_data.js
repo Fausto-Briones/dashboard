37624391c0aca2dd8dc4298a7452c2bc11827f28
Fausto-Briones <brionestucunangofausto@gmail.com>
2023-11-09 19:15:13 -0500

    Chart fixed and data section added

--- public/javascript/load_data.js
+++ public/javascript/load_data.js
@@ -74,7 +74,88 @@ let cargarPrecipitacion = () => {
 
         .catch(console.error);
   }
-  cargarOpenMeteo
+  let parseXML = (responseText) => {
+  
+    // Parsing XML
+    const parser = new DOMParser();
+    const xml = parser.parseFromString(responseText, "application/xml");
+    // Referencia al elemento `#forecastbody` del documento HTML
+
+    let forecastElement = document.querySelector("#forecastbody")
+    forecastElement.innerHTML = ''
+
+    // Procesamiento de los elementos con etiqueta `<time>` del objeto xml
+    let timeArr = xml.querySelectorAll("time")
+
+    timeArr.forEach(time => {
+        
+        let from = time.getAttribute("from").replace("T", " ")
+
+        let humidity = time.querySelector("humidity").getAttribute("value")
+        let windSpeed =  time.querySelector("windSpeed").getAttribute("value")
+        let precipitation =  time.querySelector("precipitation").getAttribute("value")
+        let pressure =  time.querySelector("pressure").getAttribute("value")
+        let cloud =  time.querySelector("clouds").getAttribute("value")
+
+        let template = `
+            <tr>
+                <td>${from}</td>
+                <td>${humidity}</td>
+                <td>${windSpeed}</td>
+                <td>${precipitation}</td>
+                <td>${pressure}</td>
+                <td>${cloud}</td>
+            </tr>
+        `
+
+        //Renderizando la plantilla en el elemento HTML
+        forecastElement.innerHTML += template;
+        })
+    
+  
+  }
+  
+  //Callback
+  let selectListener = async (event) => {
+  
+    let selectedCity = event.target.value
+
+    let cityStorage = localStorage.getItem(selectedCity);
+
+    if (cityStorage == null) {
+
+    try {
+
+        //API key
+        let APIkey = '09b41653a8b80530c8b7e1b3e0140c56'
+        let url = `https://api.openweathermap.org/data/2.5/forecast?q=${selectedCity}&mode=xml&appid=${APIkey}`
+
+        let response = await fetch(url)
+        let responseText = await response.text()
+        
+        await parseXML(responseText)
+
+    } catch (error) {
+        console.log(error)
+    }
+    
+    }else{
+        parseXML(cityStorage)
+    }
+  
+  }
+  
+  let loadForecastByCity = () => {
+  
+    //Handling event
+    let selectElement = document.querySelector("select")
+    selectElement.addEventListener("change", selectListener)
+  
+  }
+  
+  loadForecastByCity()
+
+  cargarOpenMeteo()
   cargarFechaActual()
   
   cargarPrecipitacion()
